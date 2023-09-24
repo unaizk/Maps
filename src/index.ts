dc2ee3b9095032f09d40f4dd13e8cf83792620d7
root <root@Unaislaptop>
2023-09-24 15:48:09 +0530

    added custom map so another devoloper cannot change that map properties

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import { User } from './User'
 import { Company } from './Company'
+import { CustomMap } from './CustomMAp'
 
 
-
-new google.maps.Map(document.getElementById('map') as HTMLElement,{
-    zoom : 1,
-    center :{
-        lat :0,
-        lng : 0
-    }
-
-})
+new CustomMap('map')
 
 
 
